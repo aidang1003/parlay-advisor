ab09454bdd6a368bdfb2cfe27d7c8d2ac862adb5
Aidan Gorny <aidang1003@gmail.com>
2026-02-23 22:05:01 -0700

    Use NBATeam type

--- list-teams.ts
+++ list-teams.ts
@@ -1,24 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
-import { BalldontlieAPI } from "@balldontlie/sdk";
+import { BalldontlieAPI, NBATeam } from "@balldontlie/sdk";
 dotenv.config();
 
 const api = new BalldontlieAPI({ apiKey: process.env.BALLDONTLIE_API_KEY }); // Init API client with API key from .env
 const CACHE_PATH = path.join(path.dirname(new URL(import.meta.url).pathname), "/cache/.bdl-teams.json"); // Cache location
 const CACHE_TTL_MS = 100 * 24 * 60 * 60 * 1000; // How long cached data stays fresh (100 days)
 
-interface BDLTeam {
-    id: number;
-    conference: string;
-    division: string;
-    city: string;
-    name: string;
-    full_name: string;
-    abbreviation: string;
-}
 
-async function getTeams(): Promise<BDLTeam[]> {
+async function getTeams(): Promise<NBATeam[]> {
     try {
         const teams = await api.nba.getTeams();
         return teams.data;
@@ -28,7 +19,7 @@ async function getTeams(): Promise<BDLTeam[]> {
     }
 }
 
-function updateTeamCache(teams: BDLTeam[]) {
+function updateTeamCache(teams: NBATeam[]) {
     const cacheData = {
         timestamp: Date.now(),
         teams: teams
@@ -36,7 +27,7 @@ function updateTeamCache(teams: BDLTeam[]) {
     fs.writeFileSync(CACHE_PATH, JSON.stringify(cacheData), 'utf-8');
 }
 
-function loadTeamCache(): BDLTeam[] | null {
+function loadTeamCache(): NBATeam[] | null {
     if (fs.existsSync(CACHE_PATH)) {
         const cacheContent = fs.readFileSync(CACHE_PATH, 'utf-8');
         const cacheData = JSON.parse(cacheContent);
